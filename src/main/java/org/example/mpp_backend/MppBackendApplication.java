c3e2dd5459da5a2a8381e8cc216c9ef9e4edaadf
Georgiana Petricele <115110913+georgianapetricele@users.noreply.github.com>
2024-05-09 21:30:32 +0300

    gold working

---
+++ src/main/java/org/example/mpp_backend/MppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.mpp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MppBackendApplication.class, args);
+    }
+
+}
