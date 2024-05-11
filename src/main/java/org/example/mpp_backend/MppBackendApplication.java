448f4cb3e0e059bf116b2beb7bcc116ed667ba7d
Georgiana Petricele <115110913+georgianapetricele@users.noreply.github.com>
2024-05-11 15:11:50 +0300

    json web token authentification working

--- src/main/java/org/example/mpp_backend/MppBackendApplication.java
+++ src/main/java/org/example/mpp_backend/MppBackendApplication.java
@@ -2,8 +2,11 @@ package org.example.mpp_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class MppBackendApplication {
 
     public static void main(String[] args) {
